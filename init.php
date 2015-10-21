06b96c4f7c4560520234e50a5a6efb0f29a06d76
Matthew Connelly <maff@maff.im>
2015-10-21 01:48:42 +0100

    Initial commit, add init.php with base code. Untested.

---
+++ init.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * auth_tlsclient plugin for Tiny Tiny RSS
+ * Allows for more flexible authentication via SSL/TLS Client certificates
+ * @author maff (maff@maff.scot)
+ * @copyright BSD3
+ * @version 0.01
+ */
+
+/**
+ * Inspired by the SSL client certificate support in auth_remote
+ * Requires the following variables be set by the server (with or without REDIRECT_ prefix): {{{
+ * SSL_CLIENT_VERIFY
+ * SSL_CLIENT_M_SERIAL
+ * SSL_CLIENT_S_DN
+ * SSL_CLIENT_CERT
+ *
+ * This plugin does not make full use of SSL_CLIENT_* variables set by apache2 mod_ssl
+ * due to a lack of compatibility with what is available by default in nginx.
+ * }}}
+ */
+
+class Auth_TlsClient extends Plugin implements IAuthModule {
+	//Private variables, functions {{{
+	private $host; private $base;
+	private function _log($msg, $level=E_USER_WARNING) {
+		trigger_error($msg,$level);
+	}
+	private function _resolveUser($fingerprint) {
+		$fingerprint = db_escape_string($fingerprint);
+		if(!$fingerprint) return '';
+		$result = db_query("SELECT owner_uid, content FROM ttrss_plugin_storage
+			WHERE name = 'auth_tlsclient'");
+		if (db_num_rows($result) < 1) return '';
+		$uids=array();
+		for ($i=0;$i<db_num_rows($result);$i++) {
+			$res=unserialize(db_fetch_result($result,$i,'content'));
+			if (!is_array($res)) continue;
+			if ($res['auth_tlsclient_certfp'] === $fingerprint) $uids[] = db_fetch_result($result,$i,'owner_uid');
+		}
+		if (count($uids) === 0) return '';
+		if (count($uids) > 1) {
+			trigger_error("Multiple uids found for certfp $fingerprint, bailing.", E_USER_WARNING);
+			return '';
+		}
+		$result = db_query("SELECT login FROM ttrss_users WHERE id = '".$uids[0]."'");
+		if (db_num_rows($result) === 1) return db_escape_string(db_fetch_result($result,0,'login'));
+		return '';
+	}
+	private function _getCertFP() {
+		//We expect that the underlying webserver will have validated against a known CA
+		if ($_SERVER["SSL_CLIENT_VERIFY"] !== "SUCCESS") return false;
+		//We also expect that the server passes the client certificate in PEM format
+		if (!length($_SERVER["SSL_CLIENT_CERT"])) return false;
+		//Get the fingerprint of the certificate
+		//SHA1 is used because I'm not sure if SHA256 -can- be used.
+		return openssl_x509_fingerprint($_SERVER["SSL_CLIENT_CERT"], 'sha1', false);
+	}
+	//}}}
+
+	//Base module scaffolding {{{
+	function api_version() { return 2; }
+
+	function about() {
+		return array(
+			0.01, #Version
+			"Authenticates users using SSL/TLS client certificates", #Description
+			"maff", #Author
+			true); #is system plugin
+	}
+
+	function init($host) {
+		$this->host = $host;
+		$this->base = new Auth_Base();
+		$host->add_hook($host::HOOK_AUTH_USER, $this);
+		$host->add_hook($host::HOOK_PREFS_TAB, $this);
+	}
+	//}}}
+
+	//Authentication {{{
+	function authenticate($login,$password) {
+		$fp = $this->_getCertFP();
+		$uname = $this->_resolveUser($fp);
+		if (!$uname) {
+			$this->_log("Failed to resolve client certificate with fingerprint $fp to a uid.");
+			return false;
+		}
+		$uid = $this->base->auto_create_user($uname, $password);
+		if(!$uid) return false;
+		$_SESSION["fake_login"] = $uname; $_SESSION["fake_password"] = '123789abcxyz';
+		$_SESSION["hide_hello"] = true; $_SESSION["hide_logout"] = true;
+		return $uid;
+	}
+	//}}}
+
+	//PrefsTab/Configuration {{{
+	function hook_prefs_tab($args) {
+		if ($args!="prefPrefs") return;
+		print "<div dojoType='dijit.layout.AccordionPane' title='".__('TLS Client Certificate Authentication')."'>";
+		print "<p>".__('If you have registered your certificate already, its fingerprint will appear in the box below. If you have not, use the Register button to view and confirm your certificate fingerprint.')."</p>";
+		print "<form dojoType='dijit.form.Form'>";
+		print "<script type='dojo/method' event='onSubmit' args='evt'>
+			evt.preventDefault();
+			if (this.validate()) {
+				new Ajax.Request('backend.php', {
+					parameters: dojo.objectToQuery(this.getValues()),
+					onComplete: function(transport) {
+						notify_info(transport.responseText);
+					}
+				});
+			}
+			</script>";
+		print "<input dojoType='dijit.form.TextBox' style='display:none' name='op' value='pluginhandler' />";
+		print "<input dojoType='dijit.form.TextBox' style='display:none' name='method' value='save' />";
+		print "<input dojoType='dijit.form.TextBox' style='display:none' name='plugin' value='auth_tlsclient' />";
+		print "<input dojoType='dijit.form.TextBox' name='certfp_cur' value='".$this->host->get($this, "auth_tlsclient_certfp")."' readonly />";
+		print "<input dojoType='dijit.form.TextBox' name='certfp' value='".$this->_getCertFP()."' readonly />";
+		print "<button dojoType='dijit.form.Button' onclick='dijit.byID(\"certfp\").attr(\"value\",\"\");'>".__('Clear certificate')."</button>";
+		print "<button dojoType='dijit.form.Button' type='submit'>".__('Update saved certificate')."</button>";
+		print "</form></div>";
+	}
+
+	function save() {
+		if (!isset($_POST["certfp"]) || !isset($_SESSION["uid"])) return;
+		$this->host->set($this, "auth_tlsclient_certfp", db_escape_string($_POST['certfp']));
+		echo __('Client certificate updated.');
+	}
+	//}}}
+}
+
+?>
